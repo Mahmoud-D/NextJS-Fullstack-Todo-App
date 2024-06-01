2b7975a417baaf7ad5db694706c22c52a8327731
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-06-01 19:07:54 +0300

    feat(prisma): Add seeding script to create multiple users with fake data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      email: "elsa@prisma.io",
-      name: "Elsa Prisma",
-    },
+  const user = await prisma.user.createMany({
+    data: Array.from({ length: 20 }, () => ({
+      email: faker.internet.email(),
+      name: faker.internet.userName(),
+      address: {
+        street: faker.location.street(),
+        city: faker.location.city(),
+        state: faker.location.state(),
+        zip: faker.location.zipCode(),
+      },
+    })),
   });
+
+
 }
 
 main()
