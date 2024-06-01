9f7e988d6926ea25779dde0a7c1a0742933dd5be
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-06-01 20:18:12 +0300

    feat: Add Zod schema and type for Todo validation

---
+++ validations/index.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const todosSchema = z.object({
+  title: z
+    .string()
+    .min(5, {
+      message: "Title must be at least 5 characters",
+    })
+    .max(50, { message: "Title must not exceed 50 characters" }),
+  body: z
+    .string()
+    .min(10, {
+      message: "Body must be at least 10 characters",
+    })
+    .max(80, { message: "Body must not exceed 50 characters" })
+    .optional(),
+});
+
+export type Todos = z.infer<typeof todosSchema>;
