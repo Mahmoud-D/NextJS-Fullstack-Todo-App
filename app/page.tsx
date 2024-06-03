ac1c38cd133c3f295330b1e5a151b36aea934640
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-06-03 09:55:21 +0300

    feat(page): Update Home function with AddTodo and TodoTable components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import AddTodo from "@/components/Dialog";
+import { getTodoListAcions } from "@/actions/todo.actions";
 
-export default function Home() {
+import AddTodo from "@/components/AddTodo";
+import TodoTable from "@/components/TodoTable";
+
+export default async function Home() {
+  const todos = await getTodoListAcions();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AddTodo />
+      <AddTodo
+        DialogDescriptionProp=" Make changes to your Todos here. Click save to update"
+        DialogButtonTitle="New Todo"
+        DialogTitleProp="New Todo"
+      />
+      <TodoTable todos={todos} />
     </main>
   );
 }
