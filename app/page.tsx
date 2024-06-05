582995700dd49bb55bf6bb58c64aea0ff22f8b68
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-06-05 22:30:01 +0300

    feat: Add user authentication and Pass the userId to the AddTodo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
+"use server";
+import { auth } from "@clerk/nextjs/server";
 import { getTodoListAcions } from "@/actions/todo.actions";
 
 import AddTodo from "@/components/AddTodo";
 import TodoTable from "@/components/TodoTable";
 
 export default async function Home() {
-  const todos = await getTodoListAcions();
-
+  const { userId } = auth();
+  const todos = await getTodoListAcions({ userId });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AddTodo
         DialogDescriptionProp=" Make changes to your Todos here. Click save to update"
         DialogButtonTitle="New Todo"
         DialogTitleProp="New Todo"
+        userId={userId}
       />
       <TodoTable todos={todos} />
     </main>
