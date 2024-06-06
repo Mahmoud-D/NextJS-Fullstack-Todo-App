e4438bf1a0126cb21be96686fc9ed0a80dfa7866
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-06-06 20:26:55 +0300

    feat: Update TodoTable.tsx with styling and content changes

--- components/TodoTable.tsx
+++ components/TodoTable.tsx
@@ -1,11 +1,13 @@
 "use client ";
 
+import moment from "moment";
 import TodosTableActions from "./TodosTableActions";
 import { Ttodo } from "@/types";
 
 import {
   Table,
   TableBody,
+  TableCaption,
   TableCell,
   TableFooter,
   TableHead,
@@ -17,27 +19,32 @@ import { Badge } from "./ui/badge";
 const TodoTable = ({ todos }: { todos: Ttodo[] }) => {
   return (
     <Table>
+      <TableCaption>A list of your todos.</TableCaption>
       <TableHeader>
         <TableRow>
-          <TableHead>ID</TableHead>
+          <TableHead className="w-[100px]">ID</TableHead>
           <TableHead>Title</TableHead>
-          <TableHead>Completed</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Created At</TableHead>
           <TableHead className="text-right">Actions</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        {todos.map((todo) => (
-          <TableRow key={todo.id}>
-            <TableCell className="font-medium">{todo.id}</TableCell>
-            <TableCell>{todo.title}</TableCell>
+      <TableBody className=" ">
+        {todos.map((todo, index) => (
+          <TableRow className="bg-red-800   " key={todo?.id}>
+            <TableCell className="font-medium ">{index + 1}</TableCell>
+            <TableCell className={todo.completed ? "line-through" : ""}>
+              {todo?.title}
+            </TableCell>
             <TableCell>
-              {todo.completed ? (
+              {todo?.completed ? (
                 <Badge>completed</Badge>
               ) : (
                 <Badge variant="secondary">uncompleted</Badge>
               )}
             </TableCell>
-            <TableCell className=" flex gap-2 justify-end ">
+            <TableCell>{moment(todo.createdAt).format("DD-MM-YYYY")}</TableCell>
+            <TableCell className="flex items-center justify-end space-x-2">
               <TodosTableActions todo={todo} />
             </TableCell>
           </TableRow>
@@ -45,8 +52,10 @@ const TodoTable = ({ todos }: { todos: Ttodo[] }) => {
       </TableBody>
       <TableFooter>
         <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell> {todos.length} </TableCell>
+          <TableCell colSpan={4}>Total</TableCell>
+          <TableCell className="text-right">
+            <Badge>{todos.length}</Badge>
+          </TableCell>
         </TableRow>
       </TableFooter>
     </Table>
