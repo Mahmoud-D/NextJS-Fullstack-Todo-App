67e4cb391e0ef1b37178fe2452e8aefb0edd27e0
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-06-06 20:38:25 +0300

    fix: Fix Deploy Erorr

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,36 @@
 import { PrismaClient } from "@prisma/client";
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.createMany({
-    data: Array.from({ length: 20 }, () => ({
-      email: faker.internet.email(),
-      name: faker.internet.userName(),
-      address: {
-        street: faker.location.street(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        zip: faker.location.zipCode(),
-      },
-    })),
-  });
+  //   const user = await prisma.user.createMany({
+  //     data: Array.from({ length: 20 }, () => ({
+  //       email: faker.internet.email(),
+  //       name: faker.internet.userName(),
+  //       address: {
+  //         street: faker.location.street(),
+  //         city: faker.location.city(),
+  //         state: faker.location.state(),
+  //         zip: faker.location.zipCode(),
+  //       },
+  //     })),
+  //   });
 
-  const todo = await prisma.todo.createMany({
-    data: Array.from({ length: 20 }, () => ({
-      title: faker.lorem.words(),
-      body: faker.lorem.sentence(),
-    })),
-  });
+  //   const todo = await prisma.todo.createMany({
+  //     data: Array.from({ length: 20 }, () => ({
+  //       title: faker.lorem.words(),
+  //       body: faker.lorem.sentence(),
+  //     })),
+  //   });
+  // }
+
+  main()
+    .catch(async (e) => {
+      console.error(e);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
 }
-
-main()
-  .catch(async (e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
